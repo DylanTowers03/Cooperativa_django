001661531729c3c0ac5008e7ef406184c2233b3a
DylanTowers03 <ddavidtorres@uniguajira.edu.co>
2023-05-16 20:55:09 -0500

    primer modelo

--- cooperativajfk/Apps/codeudor/admin.py
+++ cooperativajfk/Apps/codeudor/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Codeudor
 # Register your models here.
+
+class CodeudorAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Codeudor, CodeudorAdmin)
--- cooperativajfk/Apps/codeudor/models.py
+++ cooperativajfk/Apps/codeudor/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class Codeudor(models.Model):
+    cedula= models.SmallIntegerField( max_length=10)
+    nombreCodeudor = models.CharField(max_length=60, help_text="Ingrese el Nombre del Codeudor")
+    apellidoCodeudor = models.CharField(max_length=60, help_text="Ingrese el Apellido del Codeudor")
+    ciudadCodeudor = models.CharField(max_length=100, help_text="Ingrese la ciudad donde reside")
+    telefonoCodeudor = models.CharField(max_length=12, help_text="Ingrese el Telefono del Codeudor")
+    direccionCodeudor = models.CharField(max_length=100, help_text="Ingrese la Direccion del Codeudor")
+    correoCodeudor = models.EmailField(max_length=100, help_text="Ingrese el Correo del Codeudor")
+    PosibleSocio = models.BooleanField(help_text="Ingrese el Correo del Codeudor")
+  
+
+    def __str__(self):
+        return self.nombreCodeudor
+
+    class Meta:
+        verbose_name = "Codeudor"
+        verbose_name_plural = "Codeudor"
